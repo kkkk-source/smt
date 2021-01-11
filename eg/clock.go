2e610d682945b436e7e388214ee20f813f2a44cd
k <mol.tired@gmail.com>
2021-01-11 13:39:46 -0500

    add concurrent application: clock server

---
+++ eg/clock.go
@@ -0,0 +1,38 @@
+// Clock is a TCP server that periodically writes the time.
+package eg
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func ClockServer(port string) {
+	listener, err := net.Listen("tcp", "0.0.0.0:"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			// e.g, connection aborted
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			// client disconnected
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
