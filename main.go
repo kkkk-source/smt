1e8d900c8070ff877bce4ab46c3e1e2c12942c74
k <mol.tired@gmail.com>
2021-01-11 12:45:48 -0500

    smt init

---
+++ main.go
@@ -0,0 +1,10 @@
+// Concurrency Issues Simulator (smt) is a simulator that pretend
+// demonstrate bad practices and its consequences when we develop
+// concurrent programs without being aware of the concurrency issues.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, smt")
+}
