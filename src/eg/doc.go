324fee774659b70e65f141af0f510c416a0f24c3
k <mol.tired@gmail.com>
2021-01-11 17:34:43 -0500

    change documentation format

--- src/eg/doc.go
+++ src/eg/doc.go
@@ -1,6 +1,6 @@
 // eg package provide a variaty of networking programs that you can 
 // use to play with. Networking is a natural domain in which to use 
-// concurrency since servers typically handle many connections 
-// from their clients at once, each client being essentially 
-// independent of the others.
+// concurrency since servers typically handle many connections from 
+// their clients at once, each client being essentially independent 
+// of the others.
 package eg
