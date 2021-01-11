7406e3ad433b82ea474b114ea8df935694771506
k <mol.tired@gmail.com>
2021-01-11 17:32:32 -0500

    add concurrent applications: clock and echo servers

--- src/eg/doc.go
+++ src/eg/doc.go
@@ -1,4 +1,6 @@
-// Networking is a natural domain in which to use concurrency since
-// servers typically handle many connections from their clients at
-// once, each client being essentially independent of the others.
+// eg package provide a variaty of networking programs that you can 
+// use to play with. Networking is a natural domain in which to use 
+// concurrency since servers typically handle many connections 
+// from their clients at once, each client being essentially 
+// independent of the others.
 package eg
---
+++ src/eg/server.go
@@ -0,0 +1,64 @@
+package eg
+
+import (
+	"io"
+	"fmt"
+	"log"
+	"net"
+	"time"
+	"bufio"
+	"strings"
+)
+
+// MakeServer make easy to create one of this package's
+// servers, e.g, ClockServer or EchoServer.
+func MakeServer(fn func(net.Conn), port string) error {
+    if port == "" {
+        port = "8000"
+    }
+	listener, err := net.Listen("tcp", "0.0.0.0:"+port)
+	if err != nil {
+		return err
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			// connection aborted
+			log.Print(err)
+			continue
+		}
+		go fn(conn)
+	}
+	return nil
+}
+
+// ClockServer is a TCP server that periodically writes the time.
+func ClockServer(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			// client disconnected
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// EchoServe Simulate the reverberations of a real echo, with the response
+// loud at first ("WTF!"), then moderate ("Wtf!") after a delay,
+// then quiet ("wft!") before fading to nothing.
+func EchoServer(c net.Conn) {
+    input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t>> ", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t>> ", strings.Title(strings.ToLower(shout)))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t>> ", strings.ToLower(shout))
+}
