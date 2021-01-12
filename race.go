c69d9f949ec171506e3ac146bd6303a488ad13b7
k <mol.tired@gmail.com>
2021-01-12 12:52:56 -0500

    restore balance value after each execution

--- race.go
+++ race.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"sync"
 )
 
 var raceConditionDescription = `A race condition is a situation in which the program does not give the correct result for some interleavings of the operations of multiple threads. Race conditions are pernicious because they may remain latent in a program and appear infrequently, perhaps only under heavy load or when using certain compilers, platforms, orarchitectures. This makes them hard to reproduce and diagnose. 
@@ -12,35 +12,36 @@ It  is traditional to explain the seriousness of race conditions through the met
 var balance int
 
 func Deposit(amount int) {
-    balance = balance + amount
+	balance = balance + amount
 }
 
 func Balance() int {
-    return balance
+	return balance
 }
 
 func RaceConditionToPlayWith() {
-    const a = 200
-    const b = 100
-    const want = a + b
+	const a = 200
+	const b = 100
+	const want = a + b
 
-    for (true) {
-        var wg sync.WaitGroup
+	for true {
+		var wg sync.WaitGroup
 
-        wg.Add(2)
-        go func() {
-            Deposit(a)
-            wg.Done()
-        }()
-        go func() {
-            Deposit(b)
-            wg.Done()
-        }()
-        wg.Wait()
-        if got := Balance(); got != want {
-            fmt.Printf("got = %d, want = %d\n", got, want)
-            return
-        }
-        balance = 0
-    }
+		wg.Add(2)
+		go func() {
+			Deposit(a)
+			wg.Done()
+		}()
+		go func() {
+			Deposit(b)
+			wg.Done()
+		}()
+		wg.Wait()
+		if got := Balance(); got != want {
+			fmt.Printf("got = %d, want = %d\n", got, want)
+			balance = 0
+			return
+		}
+		balance = 0
+	}
 }
