28bc841b7be4c7712b7de0e1eb95db661a4b834a
k <mol.tired@gmail.com>
2021-01-20 12:29:19 -0500

    real echo would consist of independent shouts; use another thread

--- src/example.go
+++ src/example.go
@@ -139,7 +139,7 @@ func EchoServer(c net.Conn) {
 	defer c.Close()
 	input := bufio.NewScanner(c)
 	for input.Scan() {
-		echo(c, input.Text(), 1*time.Second)
+		go echo(c, input.Text(), 1*time.Second)
 	}
 }
 
